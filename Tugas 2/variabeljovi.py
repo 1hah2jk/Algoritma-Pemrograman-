ee208c62505125f80040cd225141ed7098478e81
Djovita <djovitaasmara@gmail.com>
2026-09-16 12:22:17 +0700

    Add files via upload

---
+++ Tugas 2/variabeljovi.py
@@ -0,0 +1,15 @@
+# ===========================
+# Progam Pengenalan Variabel Python
+# ===========================
+
+# Membuat Variabel 
+nama = "Djovita Asmara"
+umur = 19
+tinggi = 163
+sudah_menikah = False
+
+# Menampilkan nilai dan tipe Variabel
+print("Nama         :", nama,       "| Tipe :", type(nama))
+print("Umur         :", umur,       "|Tipe  : ", type(umur))
+print("Tinggi       :", tinggi,     "|Tipe  : ", type(tinggi))
+print("Sudah_Menikah:", sudah_menikah, "|Tipe  : ", type(sudah_menikah))
