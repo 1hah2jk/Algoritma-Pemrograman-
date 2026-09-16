36955dde116dcbe47c0b8dc4bbac2a0528fd104a
Djovita <djovitaasmara@gmail.com>
2026-09-16 12:35:01 +0700

    Add files via upload

---
+++ Tugas 1/tugas 1 Keliling Persegi.py
@@ -0,0 +1,8 @@
+# Program menghitung keliling persegi
+
+sisi = float(input("Masukkan panjang sisi persegi: "))
+
+keliling = 4 * sisi
+
+print("Panjang sisi =", sisi)
+print("Keliling persegi =", keliling)
